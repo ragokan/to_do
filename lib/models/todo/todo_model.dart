bfbbe0978d1535424e904a92571bf7d0d5703a5e
ragokan <okanyldrmop@gmail.com>
2021-03-22 15:05:47 +0300

    create todo model

---
+++ lib/models/todo/todo_model.dart
@@ -0,0 +1,11 @@
+class Todo {
+  String title;
+  String description;
+  bool isCompleted;
+
+  Todo({
+    required this.title,
+    required this.description,
+    this.isCompleted = false,
+  });
+}
