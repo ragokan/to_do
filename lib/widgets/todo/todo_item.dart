b9f1ac95395aaec500ae20e6bdd3583e78edc449
ragokan <okanyldrmop@gmail.com>
2021-03-23 10:33:07 +0300

    swipe to complete

--- lib/widgets/todo/todo_item.dart
+++ lib/widgets/todo/todo_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:to_do/modules/todo/todo_controller.dart';
 import 'package:to_do/widgets/todo/delete_todo_diagloue.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoItem extends StatelessWidget {
   final int id;
@@ -12,42 +13,67 @@ class TodoItem extends StatelessWidget {
   Widget build(_) {
     var todo = todoController.todos.firstWhere((element) => element.id == id);
     final themeData = Get.theme!;
+    final actions = todo.isCompleted
+        ? [
+            IconSlideAction(
+              caption: 'Un-complete',
+              icon: Icons.arrow_back_sharp,
+              color: Colors.red,
+              onTap: () => todoController
+                  .setTodos(() => todo.isCompleted = !todo.isCompleted),
+            )
+          ]
+        : [
+            IconSlideAction(
+              caption: 'Complete',
+              icon: Icons.done,
+              color: Colors.green,
+              onTap: () => todoController
+                  .setTodos(() => todo.isCompleted = !todo.isCompleted),
+            )
+          ];
     return Card(
       color: todo.isCompleted ? Colors.grey : themeData.primaryColor,
       elevation: 3,
       margin: EdgeInsets.only(bottom: 10),
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: Colors.white,
-          child: Text((todo.id + 1).toString(),
-              style: TextStyle(
-                  color: themeData.primaryColor, fontWeight: FontWeight.w900)),
-        ),
-        title: Text(
-          todo.title,
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
-        ),
-        subtitle: Text(
-          todo.description,
-          style: TextStyle(color: Colors.white),
-        ),
-        trailing: FittedBox(
-          child: Row(
-            children: [
-              IconButton(
-                color: Colors.white,
-                icon: Icon(Icons.edit),
-                onPressed: () {},
-                tooltip: 'Edit',
-              ),
-              SizedBox(width: 5),
-              IconButton(
-                color: Colors.white,
-                icon: Icon(Icons.delete),
-                onPressed: () => Get.dialog(DeleteTodoDialogue(id: id)),
-                tooltip: 'Delete',
-              ),
-            ],
+      child: Slidable(
+        actionPane: SlidableDrawerActionPane(),
+        actions: actions,
+        secondaryActions: actions,
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Text((todo.id + 1).toString(),
+                style: TextStyle(
+                    color: themeData.primaryColor,
+                    fontWeight: FontWeight.w900)),
+          ),
+          title: Text(
+            todo.title,
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+          ),
+          subtitle: Text(
+            todo.description,
+            style: TextStyle(color: Colors.white),
+          ),
+          trailing: FittedBox(
+            child: Row(
+              children: [
+                IconButton(
+                  color: Colors.white,
+                  icon: Icon(Icons.edit),
+                  onPressed: () {},
+                  tooltip: 'Edit',
+                ),
+                SizedBox(width: 5),
+                IconButton(
+                  color: Colors.white,
+                  icon: Icon(Icons.delete),
+                  onPressed: () => Get.dialog(DeleteTodoDialogue(id: id)),
+                  tooltip: 'Delete',
+                ),
+              ],
+            ),
           ),
         ),
       ),
